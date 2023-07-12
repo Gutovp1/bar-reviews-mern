8aa0d65f7fb31f4e6173b7668c3362b16bc8e1a9
gutovp1 <paivaav@gmail.com>
2023-07-12 19:58:59 -0300

    Add new routes and create review controller/dao

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -4,6 +4,14 @@ import RestaurantsController from "./restaurants.controller.js";
 const router = express.Router();
 
 router.route("/").get(RestaurantsController.apiGetRestaurants);
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById);
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines);
+
+router
+  .route("/review")
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview);
 
 export default router;
 
