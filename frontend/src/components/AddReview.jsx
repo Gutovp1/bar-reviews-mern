b66bd2ccc40ae26d0b58e86823a5a2224d688b86
gutovp1 <paivaav@gmail.com>
2023-08-10 01:02:51 -0300

    uselocation hook

--- frontend/src/components/AddReview.jsx
+++ frontend/src/components/AddReview.jsx
@@ -7,13 +7,13 @@ const AddReview = ({ user }) => {
   const [editing, setEditing] = useState(false);
 
   const location = useLocation();
-  console.log(location);
-  // useEffect(() => {
+
+  useEffect(() => {
     if (location.state && location.state.currentReview) {
       setEditing(true);
       setInitialReviewState(location.state.currentReview.text);
     }
-  // }, [location.state]);
+  }, [location.state]);
 
   const [review, setReview] = useState(initialReviewState);
   const [submitted, setSubmitted] = useState(false);
