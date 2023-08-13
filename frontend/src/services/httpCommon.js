b0357cb7be40fe1bb862fe0c419ae3500a105960
gutovp1 <paivaav@gmail.com>
2023-08-13 00:36:08 -0300

    Change url to mongo atlas

--- frontend/src/services/httpCommon.js
+++ frontend/src/services/httpCommon.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://localhost:5000/api/v1/restaurants",
+  // "http://localhost:5000/api/v1/restaurants",
+  "https://sa-east-1.aws.data.mongodb-api.com/app/bar-reviews-igeal/endpoint/",
   headers: {
     "Content-type": "application/json",
   },
